e4a3fb2e073c64e1cab11da3e40cf3961d0c460b
AndreiDudenkov <andrei634d@gmail.com>
2023-10-25 23:02:20 +0300

    iPhoneSE

--- app/js/accomodate/main.js
+++ app/js/accomodate/main.js
@@ -1,9 +1,8 @@
 
-function loadJSON(callback) {
+const loadJSON = (callback) => {
     const urlParams = new URLSearchParams(window.location.search);
     let langFromQuery = urlParams.get('lang') || null;
     const supportedLanguages = ['en', 'fr', 'es', 'de', 'pt', 'ja'];
-
     if (!langFromQuery) {
         const userLanguage = navigator.language || navigator.userLanguage;
         const formattedLanguage = userLanguage.replace('-', '_');
@@ -19,21 +18,15 @@ function loadJSON(callback) {
         .catch(error => console.error('Ошибка загрузки JSON: ', error));
 }
 
-
-
-
-    // Функция для обновления текстовых узлов HTML-элементов
-    function updateTextElements(jsonData) {
+const updateTextElements = (jsonData) => {
     document.getElementById("accessText").innerHTML = jsonData["Get Unlimited <br>Access"];
     document.getElementById("creationText").innerHTML = jsonData["Unlimited Art <br>Creation"];
     document.getElementById("stylesText").innerHTML = jsonData["Exclusive <br>Styles"];
     document.getElementById("avatarsText").innerHTML = jsonData["Magic Avatars <br>With 20% Off"];
     document.getElementById("yearlyAccessText").innerHTML = jsonData["YEARLY ACCESS"];
     document.getElementById("bestOfferText").innerHTML = jsonData["BEST OFFER"];
-    // // Замените {{price}} на фактическую цену
     document.getElementById("priceYearlyText").innerHTML = jsonData["Just {{price}} per year"].replace("{{price}}", "$39.99");
     document.getElementById("weeklyAccessText").innerHTML = jsonData["WEEKLY ACCESS"];
-    // // Замените {{price}} на фактическую цену
     document.getElementById("priceWeeklyText").innerHTML = jsonData["{{price}} <br>per week"].replace("{{price}}", "$0.48");
     document.getElementById("termsOfUseText").innerHTML = jsonData["Terms of Use"];
     document.getElementById("privacyPolicyText").innerHTML = jsonData["Privacy Policy"];
